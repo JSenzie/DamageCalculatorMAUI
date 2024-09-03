880bc72ba8dce8dcd039d9ed2835c3a24dcc8ca1
joseph <jsensback@gmail.com>
2024-09-03 13:43:29 -0700

    Add project files.

---
+++ DamageCalculatorMAUI/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using DamageCalculator;
+
+namespace DamageCalculatorMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        SwordDamage swordDamage = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            swordDamage.Magic = (Magic.IsChecked);
+            swordDamage.Flaming = (Flaming.IsChecked);
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            swordDamage.Roll = Random.Shared.Next(1, 19);
+            DisplayDamage();
+        }
+
+        private void DisplayDamage() {
+            Damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            RollDice();
+        }
+
+        private void Flaming_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            swordDamage.Flaming = (e.Value);
+            DisplayDamage();
+        }
+
+        private void Magic_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            swordDamage.Magic = (e.Value);
+            DisplayDamage();
+        }
+    }
+
+}
---
+++ DamageCalculatorMAUI/SwordDamage.cs
@@ -0,0 +1,65 @@
+﻿namespace DamageCalculator
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        
+        public int Damage {  get; private set; }
+
+        private int _roll;
+        private bool _flaming;
+        private bool _magic;
+
+        public SwordDamage()
+        {
+            CalculateDamage();
+        }
+
+        public bool Flaming
+        {
+            get => _flaming;
+            set
+            {
+                _flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        public bool Magic
+        {
+            get => _magic;
+            set
+            {
+                _magic = value;
+                CalculateDamage();
+            }
+        }
+
+        public int Roll
+        {
+            get => _roll; set
+            {
+                _roll = value;
+                CalculateDamage();
+            }
+        }   
+
+        private void CalculateDamage()
+        {
+            decimal dmg = Roll;
+
+            if (_magic) {
+                dmg *= 1.75M;
+            }
+
+            if (_flaming)
+            {
+                dmg += FLAME_DAMAGE;
+            }
+
+            Damage = (int)dmg + BASE_DAMAGE;
+        }
+  
+    }
+}
